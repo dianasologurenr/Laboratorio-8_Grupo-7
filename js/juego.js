b738e1b77aa718ce16bdc8146805491aae6a1a7e
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-06-15 00:34:41 -0500

    validacion juegos

--- js/juego.js
+++ js/juego.js
@@ -2,9 +2,9 @@ $("#juegoform").submit(function(e) {
     e.preventDefault();
     
     var nombre = $("#nombre").val();
-
-    console.log(nombre);
-    console.log(nombre.length);
+    var imagen = $("#imagen").val();
+    var distribuidora = $("#distribuidora").val();
+    var plataforma = $("#plataforma").val();
 
     if((nombre.length <3 || nombre.length>50)){
         $("#error-nombre").show(500).delay(1000).hide(500);
@@ -14,6 +14,33 @@ $("#juegoform").submit(function(e) {
             $("#nombre").removeClass('is-invalid');
         }, 2000);
     } 
+
+
+    // Rodrigo estuvo aquí :V
+
+    if ((imagen.length < 10 || imagen.length> 256)) {
+        $("#error-imagen").show(500).delay(1000).hide(500);
+        $("#imagen").addClass("is-invalid");
+        setTimeout(function() {
+            $("#imagen").removeClass('is-invalid');
+        }, 2000);
+    }
+
+    if (plataforma === "") {
+        $("#error-plataforma").show(500).delay(1000).hide(500);
+        $("#plataforma").addClass("is-invalid");
+        setTimeout(function() {
+            $("#plataforma").removeClass('is-invalid');
+        }, 2000);
+    }
+
+    if (distribuidora === "") {
+        $("#error-distribuidora").show(500).delay(1000).hide(500);
+        $("#distribuidora").addClass("is-invalid");
+        setTimeout(function() {
+            $("#distribuidora").removeClass('is-invalid');
+        }, 2000);
+    }
 
     
 

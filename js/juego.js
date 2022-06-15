60ffdcc5230a7a42f58216d5b287d945bce9be2a
dianasr <sologurenr.d@pucp.edu.pe>
2022-06-15 00:36:54 -0500

    form juegos avances

--- js/juego.js
+++ js/juego.js
@@ -2,16 +2,27 @@ $("#juegoform").submit(function(e) {
     e.preventDefault();
     
     var nombre = $("#nombre").val();
+    var descripcion = $("#descripcion").val();
+    var genero = $("#genero").val();
+    var precio = $("#precio").val();
+
+
 
     console.log(nombre);
     console.log(nombre.length);
 
     if((nombre.length <3 || nombre.length>50)){
         $("#error-nombre").show(500).delay(1000).hide(500);
-        console.log("holi");
         $("#nombre").addClass("is-invalid");
         setTimeout(function() {
             $("#nombre").removeClass('is-invalid');
+        }, 2000);
+    }
+    if((descripcion.length <3 || descripcion.length>512)){
+        $("#error-nombre").show(500).delay(1000).hide(500);
+        $("#descripcion").addClass("is-invalid");
+        setTimeout(function() {
+            $("#descripcion").removeClass('is-invalid');
         }, 2000);
     } 
 

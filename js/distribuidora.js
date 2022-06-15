272d14b4733d7c99fccdd393bbc0a8d1f6d90aaa
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2022-06-15 02:23:55 -0500

    validacion y agregar distrubucion

--- js/distribuidora.js
+++ js/distribuidora.js
@@ -1,3 +1,5 @@
+var index=1;
+
 $("#juegoform").submit(function(e) {
     e.preventDefault();
     
@@ -10,6 +12,8 @@ $("#juegoform").submit(function(e) {
     console.log(nombre);
     console.log(nombre.length);
 
+    var llave=0;
+
     if((nombre.length <3 || nombre.length>50)){
         $("#error-nombre").show(500).delay(1000).hide(500);
         console.log("holi");
@@ -17,6 +21,7 @@ $("#juegoform").submit(function(e) {
         setTimeout(function() {
             $("#nombre").removeClass('is-invalid');
         }, 2000);
+        llave++;
     } 
 
     if((descripcion.length <3 || descripcion.length>512)){
@@ -26,6 +31,7 @@ $("#juegoform").submit(function(e) {
         setTimeout(function() {
             $("#descripcion").removeClass('is-invalid');
         }, 2000);
+        llave++;
     } 
 
     if(web.length<3 || web.length>198){
@@ -35,6 +41,7 @@ $("#juegoform").submit(function(e) {
         setTimeout(function() {
             $("#web").removeClass('is-invalid');
         }, 2000);
+        llave++;
     } 
 
     if( fundacion<1800 || fundacion>2100){
@@ -44,6 +51,7 @@ $("#juegoform").submit(function(e) {
         setTimeout(function() {
             $("#fundacion").removeClass('is-invalid');
         }, 2000);
+        llave++;
     } 
 
     if(pais === ""){
@@ -52,7 +60,25 @@ $("#juegoform").submit(function(e) {
         setTimeout(function() {
             $("#pais").removeClass('is-invalid');
         }, 4000);
+        llave++;
     } 
+
+    if(llave===0){
+        let distribuidora=  '<tr>\n' +
+'                                <td>'+ index+ ' </td>\n' +
+'                                <td>' + nombre + '</td>\n' +
+'                                <td>' + descripcion + '</td>\n' +
+'                                <td>' + web + '</td>\n' +
+'                                <td>' + fundacion + '</td>\n' +
+'                                <td>' + pais + '</td>\n' +
+'                                <td><a class="btn btn-primary" href="#"><span class="bi bi-pencil-square"></span> Editar </a></td>\n' +
+'                                <td><a class="btn btn-danger" href="#"><span class="bi bi-trash"></span> Borrar</a></td>\n' +
+'                            </tr>'
+        $("#distribuidores-container").append(distribuidora)
+        index++;
+    }
+
+    
 
 
     

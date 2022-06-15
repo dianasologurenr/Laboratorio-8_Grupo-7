2a3792fa393bed394d8b0a0859051d8b540f15de
Leo <a20185896@pucp.edu.pe>
2022-06-15 13:42:51 -0500

    plataforma

--- js/plataforma.js
+++ js/plataforma.js
@@ -1,16 +1,18 @@
-var index=0;
+var plataformas = []
+var id = 0
+var edit = false
+var idedit = -1
+
 $("#plataformaform").submit(function(e) {
     e.preventDefault();
-
-    var lista=[];
     
-    var nombre = $("#nombre").val();
-    var descripcion = $("#descripcion").val();
-    var web = $("#web").val();
+    let nombre = $("#nombre").val();
+    let descripcion = $("#descripcion").val();
+    let web = $("#web").val();
 
-    var validacionnombre=nombre.length <3 || nombre.length>20;
-    var validaciondescripcion=descripcion.length <3 || descripcion.length>512;
-    var validacionweb=web.length <3 || web.length>256
+    let validacionnombre=nombre.length <3 || nombre.length>20;
+    let validaciondescripcion=descripcion.length <3 || descripcion.length>512;
+    let validacionweb=web.length <3 || web.length>256
 
     if(validacionnombre){
         $("#error-nombre").show(500).delay(3000).hide(500);
@@ -37,28 +39,54 @@ $("#plataformaform").submit(function(e) {
     }
 
     if(!validacionnombre && !validaciondescripcion && !validacionweb){
-        let object={
-            id:index,
-            nombrep:nombre,
-            descripcionp:descripcion,
-            webp:web
-        }
-        index+=1;
-        lista.push(object);
-        $(".tabla").append(
-            '<tr>'+
-                            '<td class="text-center">'+(parseInt(object.id,10) + 1)+'</td>'+
-                            '<td class="text-center">'+object.nombrep+'</td>'+
-                            '<td class="text-center">'+object.descripcionp+'</td>'+
-                            '<td class="text-center">'+object.webp+'</td>'+
-                            '<td class="text-center"><a class="btn btn-primary"  ><span class="bi bi-pencil-square"></span></a></td>' +
-                            '<td class="text-center"><button class="btn btn-danger" ><span class="bi bi-trash"></span></button></td>'
-                            +'</tr>');
         
+        let platObject={
+            nombre: nombre,
+            descripcion: descripcion,
+            web: web
+        }
 
-                            $(".btn-borrar").click(function () {
-                                $(this).parent().parent().remove();
-                            })
+        if (edit) {
+            plataformas[idedit] = platObject
+            $("#" + idedit).replaceWith(genRow(idedit,nombre,descripcion,web));
+        } else {
+            let plataforma = genRow(id,nombre,descripcion,web)
+            $("#msg-creado").show(500).delay(3000).hide(500)
+            $("#plataforma-container").append(plataforma)
+            plataformas.push(platObject);
+        
+        }
+
+        $('#plataformaform').trigger("reset")
+        $(".btn-borrar").click(function () {
+            $(this).parent().parent().remove();
+        })
+
+        id = id + 1
     }
-});
+})
 
+$('#btn-cancelar').click(function () {
+    $('#plataformaform').trigger('reset');
+})
+
+function editarPlataforma(idx) {
+    let obj = plataformas[idx]
+    $("#nombre").val(obj.nombre)
+    $("#descripcion").val(obj.descripcion);
+    $("#web").val(obj.web);
+    edit = true
+    idedit = idx
+}
+
+function genRow(id,nombre,descr,web ) {
+    let plataforma = '<tr id="' + id + '">\n'+
+    '<td>'+id+'</td>\n'+
+    '<td>'+nombre+'</td>\n'+
+    '<td>'+descr+'</td>\n'+
+    '<td>'+web+'</td>\n'+
+    '<td><a class="btn btn-primary" onclick="editarPlataforma(' + id + ')"><span class="bi bi-pencil-square"></span> Editar</a></td>\n' +
+    '<td><button class="btn btn-danger btn-borrar"><span class="bi bi-trash"></span> Borrar</button></td>\n' +
+    '</tr>'
+    return plataforma
+}
